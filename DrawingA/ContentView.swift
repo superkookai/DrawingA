b698448909c8cb18f99f9284df8cc851b51e7913
superkookai <weerawut@live.com>
2026-01-06 09:24:25 +0700

    Initial Commit

---
+++ DrawingA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrawingA
+//
+//  Created by Weerawut on 6/1/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrawingA/DrawingAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAApp.swift
+//  DrawingA
+//
+//  Created by Weerawut on 6/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
